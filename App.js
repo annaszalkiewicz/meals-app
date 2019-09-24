d97559ac05a6a5093ca4750648cb31776f416219
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-24 23:36:39 +0200

    Create categories screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CategoriesScreen />
     </View>
   );
 }
