29afb10ec2520898a86ec3afe8573cc047a279fa
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-25 22:50:41 +0200

    Add styling to category item

--- components/CategoryItem.js
+++ components/CategoryItem.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-import BodyText from "../components/ui/BodyText";
+import HeadingOne from "../components/ui/HeadingOne";
 
 const CategoryItem = props => {
   return (
-    <View>
-      <BodyText>{props.title}</BodyText>
+    <View style={{ ...styles.container, backgroundColor: props.color }}>
+      <HeadingOne>{props.title}</HeadingOne>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "50%",
+    height: 150,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 14
+  }
+});
 
 export default CategoryItem;
