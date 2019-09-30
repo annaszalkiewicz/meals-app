906905bb426d8b49aa477d251421a9e811c0f87f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-01 00:11:51 +0200

    Add content to meal item

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, ImageBackground, StyleSheet } from "react-native";
+
+import BodyText from './ui/BodyText';
+import HeadingOne from './ui/HeadingOne';
 
 const MealItem = props => {
   return (
     <TouchableOpacity>
+      <View>
         <View>
-          <Text>{props.item.title}</Text>
+          <ImageBackground source={{uri: props.item.imageUrl}} style={styles.image}>
+            <View>
+            <HeadingOne>{props.item.title}</HeadingOne>
+            </View>
+          </ImageBackground>
         </View>
-      </TouchableOpacity>
-  )
-}
+        <View>
+          <BodyText>{props.item.duration}m</BodyText>
+          <BodyText>{props.item.complexity}</BodyText>
+          <BodyText>{props.item.affordability}</BodyText>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-  
+  image: {
+    width: 300,
+    height: 200
+  }
 });
 export default MealItem;
