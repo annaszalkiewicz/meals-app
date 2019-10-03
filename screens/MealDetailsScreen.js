ea52ebf6ad0748a24a892b490bac57ba876b38ac
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-03 22:14:05 +0200

    Display heart outline icon in header

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/meals-db";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailsScreen = props => {
   const mealId = props.navigation.getParam("mealId");
@@ -23,7 +25,12 @@ MealDetailsScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam("mealId");
   const currentMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    title: currentMeal.title
+    title: currentMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Add to favorites" iconName="heart-outline" />
+      </HeaderButtons>
+    )
   };
 };
 
