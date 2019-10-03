899b6f92a4fe2a04fe8c777b5c3bd072fc3330e6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-03 22:27:28 +0200

    Create bottom tabs navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,31 +1,41 @@
-import { Platform } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import CategoriesScreen from '../screens/CategoriesScreen';
-import MealCategoryScreen from '../screens/MealCategoryScreen';
+import CategoriesScreen from "../screens/CategoriesScreen";
+import MealCategoryScreen from "../screens/MealCategoryScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
-import variables from '../constants/variables';
+import FavoritesScreen from "../screens/FavoritesScreen";
+import variables from "../constants/variables";
 
-const MealsNavigator = createStackNavigator({
-  Categories: {
-    screen: CategoriesScreen,
-    navigationOptions: {
-      title: 'Meal Categories'
-    }
-  },
-  MealCategory: MealCategoryScreen,
-  MealDetails: MealDetailsScreen
-}, {
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: Platform.OS === 'ios' ? '#fff' : "rgba(0,0,0,0.8)"
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: {
+        title: "Meal Categories"
+      }
     },
-    headerTintColor: Platform.OS === 'ios' ? '#000' : "#fff",
-    headerTitleStyle: {
-      fontFamily: variables.ralewaySemibold
+    MealCategory: MealCategoryScreen,
+    MealDetails: MealDetailsScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "ios" ? "#fff" : "rgba(0,0,0,0.8)"
+      },
+      headerTintColor: Platform.OS === "ios" ? "#000" : "#fff",
+      headerTitleStyle: {
+        fontFamily: variables.ralewaySemibold
+      }
     }
   }
+);
+
+const TabsNavigator = createBottomTabNavigator({
+  AllMeals: MealsNavigator,
+  Favorites: FavoritesScreen
 });
 
-export default createAppContainer(MealsNavigator);
+export default createAppContainer(TabsNavigator);
