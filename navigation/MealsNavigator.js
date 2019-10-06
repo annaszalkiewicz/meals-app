0a32d79feb26aa868d418af191367ec2ed8fe0b4
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-06 21:17:36 +0200

    Create favorites stack navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import MealCategoryScreen from "../screens/MealCategoryScreen";
@@ -12,69 +12,84 @@ import MealDetailsScreen from "../screens/MealDetailsScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import variables from "../constants/variables";
 
-const MealsNavigator = createStackNavigator(
-  {
-    Categories: {
-      screen: CategoriesScreen,
-      navigationOptions: {
-        title: "Meal Categories"
-      }
+const defaultStackNavigationOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === "ios" ? "#fff" : "rgba(0,0,0,0.8)"
     },
-    MealCategory: MealCategoryScreen,
-    MealDetails: MealDetailsScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "ios" ? "#fff" : "rgba(0,0,0,0.8)"
-      },
-      headerTintColor: Platform.OS === "ios" ? "#000" : "#fff",
-      headerTitleStyle: {
-        fontFamily: variables.ralewaySemibold
-      }
+    headerTintColor: Platform.OS === "ios" ? "#000" : "#fff",
+    headerTitleStyle: {
+      fontFamily: variables.ralewaySemibold
     }
   }
-);
+};
+
+const MealsNavigator = createStackNavigator({
+  Categories: {
+    screen: CategoriesScreen,
+    navigationOptions: {
+      title: "Meal Categories"
+    }
+  },
+  MealCategory: MealCategoryScreen,
+  MealDetails: MealDetailsScreen
+}, defaultStackNavigationOptions);
+
+
+const FavoritesNavigator = createStackNavigator({
+  Favorites: FavoritesScreen,
+  MealDetails: MealDetailsScreen
+}, defaultStackNavigationOptions);
+
 
 const tabsConfig = {
   AllMeals: {
     screen: MealsNavigator,
     navigationOptions: {
-      tabBarLabel: 'Meals',
+      tabBarLabel: "Meals",
       tabBarIcon: tabInfo => {
-        return <MaterialCommunityIcons name="food" size={24} color={tabInfo.tintColor} />
+        return (
+          <MaterialCommunityIcons
+            name="food"
+            size={24}
+            color={tabInfo.tintColor}
+          />
+        );
       },
-      tabBarColor: 'darkred'
+      tabBarColor: "darkred"
     }
   },
   Favorites: {
-    screen: FavoritesScreen,
+    screen: FavoritesNavigator,
     navigationOptions: {
-      tabBarLabel: 'Favorites',
+      tabBarLabel: "Favorites",
       tabBarIcon: tabInfo => {
-        return <MaterialCommunityIcons name="heart" size={24} color={tabInfo.tintColor} />
+        return (
+          <MaterialCommunityIcons
+            name="heart"
+            size={24}
+            color={tabInfo.tintColor}
+          />
+        );
       },
-      tabBarColor: 'red'
+      tabBarColor: "red"
     }
   }
 };
 
-const TabsNavigator = Platform.OS === 'android' ? createMaterialBottomTabNavigator(
-  tabsConfig,
-  {
-    shifting: true,
-    barStyle: {
-      backgroundColor: '#333'
-    }
-  }
-) : createBottomTabNavigator(
-  tabsConfig,
-  {
-    tabBarOptions: {
-      activeTintColor: "red",
-      inactiveTintColor: "rgba(0,0,0,0.6)"
-    }
-  }
-);
+const TabsNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(tabsConfig, {
+        shifting: true,
+        barStyle: {
+          backgroundColor: "#333"
+        }
+      })
+    : createBottomTabNavigator(tabsConfig, {
+        tabBarOptions: {
+          activeTintColor: "red",
+          inactiveTintColor: "rgba(0,0,0,0.6)"
+        }
+      });
 
 export default createAppContainer(TabsNavigator);
