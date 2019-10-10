a94f424e5bc6c5e101b17386714060c3a9a106f6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-10-10 23:09:57 +0200

    Edit switch: add value, onValueChange, trackColor & thumColor

--- components/FilterSwitch.js
+++ components/FilterSwitch.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { View, Switch, StyleSheet } from 'react-native'
-import HeadingOne from './ui/HeadingOne'
+import React from "react";
+import { View, Switch, StyleSheet, Platform } from "react-native";
+import HeadingOne from "./ui/HeadingOne";
 
 const FilterSwitch = props => {
   return (
     <View style={styles.container}>
-      <HeadingOne style={styles.heading}>{props.children}</HeadingOne>
-      <Switch />
+      <HeadingOne style={styles.heading}>{props.label}</HeadingOne>
+      <Switch
+        value={props.value}
+        onValueChange={props.onChange}
+        trackColor={{ true: "darkred" }}
+        thumbColor={Platform.OS === "android" ? "darkred" : ""}
+      />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
     padding: 30,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   heading: {
     fontSize: 18
   }
 });
 
-export default FilterSwitch
+export default FilterSwitch;
