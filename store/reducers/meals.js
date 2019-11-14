5de0645939941fe6a251d74741c69886795e9d94
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-11-14 23:54:13 +0100

    Add TOGGLE_FAVORITE case to meal reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,13 +1,36 @@
 import { MEALS } from '../../data/meals-db';
+import { TOGGLE_FAVORITE } from '../actions/meals';
 
 const initialState = {
-  meals: MEALS,
-  filteredMeals: MEALS,
-  favoriteMeals: []
-}
+	meals: MEALS,
+	filteredMeals: MEALS,
+	favoriteMeals: []
+};
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
-}
+	switch (action.type) {
+		case TOGGLE_FAVORITE:
+			const existingIndex = state.favoriteMeals.findIndex(
+				meal => meal.id === action.mealId
+			);
+			if (checkIfExistIndex >= 0) {
+				const updatedFavMeals = [...state.favoriteMeals];
+				updatedFavMeals.splice(existingIndex, 1);
+				return {
+					...state,
+					favoriteMeals: updatedFavMeals
+				};
+			} else {
+				const newFav = state.meals.find(meal => meal.id === action.mealId);
+				return {
+					...state,
+					favoriteMeals: state.favoriteMeals.concat(newFav)
+				};
+			}
+
+		default:
+			return state;
+	}
+};
 
 export default mealsReducer;
