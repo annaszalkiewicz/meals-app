ce1388211c8d76f3ae249d0934293e601240ec26
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-11-15 22:17:47 +0100

    Add message if no meal added to fvorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,28 +1,32 @@
-import React from "react";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import React from 'react';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
-import MealsList from "../components/MealsList";
-import HeaderButton from "../components/HeaderButton";
+import MealsList from '../components/MealsList';
+import HeaderButton from '../components/HeaderButton';
+import BodyText from '../components/ui/BodyText';
 
 const FavoritesScreen = props => {
-  const favMeals = useSelector(state => state.meals.favoriteMeals)
-  return <MealsList navigation={props.navigation} listData={favMeals} />;
+	const favMeals = useSelector(state => state.meals.favoriteMeals);
+	if (favMeals.length === 0) {
+		return <BodyText>No meal added to favorites</BodyText>;
+	}
+	return <MealsList navigation={props.navigation} listData={favMeals} />;
 };
 
 FavoritesScreen.navigationOptions = data => {
-  return {
-    title: "Favorites Meals",
-    headerLeft: (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title="Menu"
-          iconName="menu"
-          onPress={() => data.navigation.toggleDrawer()}
-        />
-      </HeaderButtons>
-    )
-  };
+	return {
+		title: 'Favorites Meals',
+		headerLeft: (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Menu'
+					iconName='menu'
+					onPress={() => data.navigation.toggleDrawer()}
+				/>
+			</HeaderButtons>
+		)
+	};
 };
 
 export default FavoritesScreen;
