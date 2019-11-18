7c96293771a57c97dbdf86c5817b267ccff14d81
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-11-18 22:23:37 +0100

    Display message if no filtered meals found

--- screens/MealCategoryScreen.js
+++ screens/MealCategoryScreen.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
-import { CATEGORIES } from "../data/category-db";
-import MealsList from "../components/MealsList";
+import { CATEGORIES } from '../data/category-db';
+import MealsList from '../components/MealsList';
+import BodyText from '../components/ui/BodyText';
 
 const MealCategoryScreen = props => {
-  const categoryId = props.navigation.getParam("categoryId");
-  const availableMeals = useSelector(state => state.meals.filteredMeals)
-  const mealsInCategory = availableMeals.filter(
-    meal => meal.categoryIds.indexOf(categoryId) >= 0
-  );
+	const categoryId = props.navigation.getParam('categoryId');
+	const availableMeals = useSelector(state => state.meals.filteredMeals);
+	const mealsInCategory = availableMeals.filter(
+		meal => meal.categoryIds.indexOf(categoryId) >= 0
+	);
 
-  return <MealsList navigation={props.navigation} listData={mealsInCategory} />;
+	if (mealsInCategory.length === 0) {
+		return (
+			<View style={styles.container}>
+				<BodyText>No meal found. Maybe try other filters?</BodyText>
+			</View>
+		);
+	}
+
+	return <MealsList navigation={props.navigation} listData={mealsInCategory} />;
 };
 
 MealCategoryScreen.navigationOptions = navigationData => {
-  const categoryId = navigationData.navigation.getParam("categoryId");
-  const currentCategory = CATEGORIES.find(c => c.id === categoryId);
-  return {
-    title: currentCategory.title
-  };
+	const categoryId = navigationData.navigation.getParam('categoryId');
+	const currentCategory = CATEGORIES.find(c => c.id === categoryId);
+	return {
+		title: currentCategory.title
+	};
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
 
 export default MealCategoryScreen;
